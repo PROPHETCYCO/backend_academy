7c6acd40062a5b6fed22cae4c061da7f1c2d097d
srijanibanerje <srijaniofficial2022@gmail.com>
2026-02-28 16:36:34 +0530

    onlyadmincontroller

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -24,7 +24,7 @@ export const getDashboardStats = async (req, res) => {
         // 3️⃣ Current month amount
         const now = new Date();
         const startOfMonth = new Date(now.getFullYear(), now.getMonth(), 1);
-        const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+        const endOfMonth = new Date(now.getFullYear(), now.getMonth() + 1, 0 , 23, 59 , 59, 999);
 
         const currentMonthData = await Checkout.aggregate([
             {
